99517e23435fc83fdda550d01d4866db1e2468bd
Nikunj <mnikunj262@gmail.com>
2025-10-14 01:56:53 +0530

    update: navbar with burger line

--- wing/app/page.tsx
+++ wing/app/page.tsx
@@ -1,20 +1,11 @@
-import { Meteors } from "@/components/ui/meteors";
-import Image from "next/image";
-import logo from "@/public/logo.svg"
+import Feature from "@/components/home/Feature";
+import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <Meteors number={15} />
-      <div className=" min-h-screen flex justify-center items-center">
-        <p className="text-5xl sm:text-7xl md:text-8xl lg:text-9xl w-3/4 sm:w-2/3 font-bold text-center text-balance capitalize">
-
-          Fund give you {' '}
-          <span className="inline-block -space-x-4 sm:-space-x-6 md:-space-x-8 lg:-space-x-10">
-            <span className="z-10">wings</span> <Image src={logo} className="w-7 h-7 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-13 lg:h-13 lg:top-5 md:top-2 relative md:right-1 -z-10 inline-block align-top" alt={'logo'}></Image>
-          </span>
-        </p>
-      </div>
+    <div>
+      <Hero />
+      <Feature />
     </div>
   );
 }
--- wing/components/ui/Navbar.tsx
+++ wing/components/ui/Navbar.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Image from "next/image"
 import logo from '@/public/logo.svg';
-
+import { Slash } from 'lucide-react';
+import { useState } from "react";
 export const Navbar = () => {
+  const [tap, setTap] = useState<boolean>(false);
   return (
-    <div className="border border-zinc-800 py-2.5 px-10  flex justify-between items-center fixed max-w-screen min-w-screen top-0">
+    <div className="border border-zinc-800 py-2.5 px-3 sm:px-10 flex justify-between items-center fixed max-w-screen min-w-screen top-0">
       <div className={`h-10 w-10`}>
         <Image
           src={logo}
@@ -12,8 +15,14 @@ export const Navbar = () => {
           height={200}
         />
       </div>
-      <div className={``}>
-        2
+      <div className={`flex  items-center relative px-12`}>
+        <div className="">
+          Connect
+        </div>
+        <div className={`${tap ? '-space-y-6' : '-space-y-7'} absolute right-0 cursor-pointer transition-all duration-300 p-1 flex flex-col justify-center overl`} onClick={() => setTap(pre => !pre)}>
+          <Slash className={`${tap ? 'rotate-90' : 'rotate-45 h-10'} transition-all duration-300`} />
+          <Slash className={`${tap ? 'rotate-0' : 'rotate-45 h-10'} transition-all duration-300`} />
+        </div>
       </div>
     </div>
   )
