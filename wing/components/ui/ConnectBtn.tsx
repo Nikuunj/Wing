6e06efb3702b4416a8eb247395cc5f5677c8e907
Nikunj <mnikunj262@gmail.com>
2025-10-18 01:43:13 +0530

    fix: hydration error

--- wing/components/ui/ConnectBtn.tsx
+++ wing/components/ui/ConnectBtn.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 function ConnectBtn({ children, className }: { children: ReactNode, className?: string }) {
   return (
-    <button key='ConnectBtn' className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+    <div key='ConnectBtn' className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
       <span className="absolute inset-0 overflow-hidden rounded-full">
         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
       </span>
@@ -25,7 +25,7 @@ function ConnectBtn({ children, className }: { children: ReactNode, className?: 
         </svg>
       </div>
       <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
-    </button>
+    </div>
   )
 }
 
