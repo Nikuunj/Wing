b6236900735beab48033349d5f657d7cef844a4a
Nikunj <mnikunj262@gmail.com>
2025-10-18 03:37:58 +0530

    w/address

--- wing/app/w/[address]/page.tsx
+++ wing/app/w/[address]/page.tsx
@@ -1,12 +1,16 @@
 import Profile from "@/components/w/Profile"
-import Tip from "@/components/w/Tip"
+import Tip from "@/components/w/TipForm"
 
 async function TipPage({ params }: { params: Promise<{ address: string }> }) {
   const address = (await params).address
   return (
-    <div className="flex flex-col md:flex-row gap-4 min-h-screen justify-center items-center overflow-hidden">
-      <Profile address={address} />
-      <Tip address={address} />
+    <div className="grid grid-cols-3 gap-4 justify-center items-start translate-y-15 overflow-hidden min-h-[92vh] ">
+      <div className="border-r border-zinc-700 col-span-1 h-full">
+        <Profile address={address} />
+        <Tip address={address} />
+      </div>
+      <div className="bg-red-300 col-span-2">
+      </div>
     </div>
   )
 }
---
+++ wing/components/w/TipForm.tsx
@@ -0,0 +1,9 @@
+function Tip({ address }: { address: string }) {
+  return (
+    <div className="break-words px-7 py-8 ">
+      Tip {address}
+    </div>
+  )
+}
+
+export default Tip
