78a76ec58155b41bfd34b5cecc7ff5e40fd2cc31
Nikunj <mnikunj262@gmail.com>
2025-10-20 00:47:51 +0530

    address page

--- wing/app/w/[address]/page.tsx
+++ wing/app/w/[address]/page.tsx
@@ -1,17 +1,11 @@
 import Profile from "@/components/w/Profile"
 import Tip from "@/components/w/TipForm"
+import Wpage from "@/components/w/Wpage"
 
 async function TipPage({ params }: { params: Promise<{ address: string }> }) {
   const address = (await params).address
   return (
-    <div className="grid grid-cols-3 gap-4 justify-center items-start translate-y-15 overflow-hidden min-h-[92vh] ">
-      <div className="border-r border-zinc-700 col-span-1 h-full">
-        <Profile address={address} />
-        <Tip address={address} />
-      </div>
-      <div className="bg-red-300 col-span-2">
-      </div>
-    </div>
+    <Wpage address={address} />
   )
 }
 
