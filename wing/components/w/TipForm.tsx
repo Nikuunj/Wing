a08eaf978ed57ca0c7ae4caaf7d6f3e14dfed36c
Nikunj <mnikunj262@gmail.com>
2025-10-20 17:12:27 +0530

    fix: wpage and tip form

--- wing/components/w/TipForm.tsx
+++ wing/components/w/TipForm.tsx
@@ -16,31 +16,37 @@ function Tip({ address, mintAddress }: { address: string, mintAddress: string })
     <div className="px-7 py-8 row-span-4 h-fit   space-y-5 flex flex-col-reverse sm:flex-row lg:flex-col-reverse">
       <div className="space-y-7 flex flex-col justify-center items-center sm:items-start  w-full">
         <h1 className="text-4xl sm:text-5xl  font-bold">Funds gives Wings</h1>
-        <InputBox
-          refrence={(e) => { reference.current[0] = e }}
-          typeOfIn="text"
-          placeHolder="Name"
-        />
-        <InputBox
-          refrence={(e) => { reference.current[0] = e }}
-          typeOfIn="text"
-          placeHolder="Message"
-        />
-        <div className="relative w-fit lg:w-full">
-          <button
-            className=" px-4 py-3 min-w-72  cursor-text  border 
-          lg:w-full  outline-0 border-zinc-700 border-dotted "
-            onClick={handleSubmit}
-          >
-            Wing
-          </button>
-          <div className="absolute top-0 left-0 h-1.5 w-1.5 border-t border-l" />
-          <div className="absolute bottom-0 left-0 h-1.5 w-1.5 border-b border-l" />
-          <div className="absolute top-0 right-0  h-1.5 w-1.5 border-t border-r" />
-          <div className="absolute bottom-0 right-0 h-1.5 w-1.5 border-b border-r" />
+        <div className="space-y-7 flex flex-col items-center w-full">
+          <InputBox
+            refrence={(e) => { reference.current[0] = e }}
+            typeOfIn="text"
+            placeHolder="Name"
+          />
+          <InputBox
+            refrence={(e) => { reference.current[0] = e }}
+            typeOfIn="text"
+            placeHolder="Message"
+          />
+          <InputBox
+            refrence={(e) => { reference.current[0] = e }}
+            typeOfIn="number"
+            placeHolder="Amount"
+          />
+
+          <div className="relative flex justify-center w-fit">
+            <button
+              className=" px-4 py-3 min-w-44 w-fit cursor-text  border  outline-0 border-zinc-700 border-dotted "
+              onClick={handleSubmit}
+            >
+              Wing
+            </button>
+            <div className="absolute top-0 left-0 h-1.5 w-1.5 border-t border-l" />
+            <div className="absolute bottom-0 left-0 h-1.5 w-1.5 border-b border-l" />
+            <div className="absolute top-0 right-0  h-1.5 w-1.5 border-t border-r" />
+            <div className="absolute bottom-0 right-0 h-1.5 w-1.5 border-b border-r" />
+          </div>
         </div>
       </div>
-
       <div className="w-full sm:flex justify-center items-center ">
         <Image src={logo} alt="logo" className="h-full w-full lg:max-w-44 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" />
       </div>
--- wing/components/w/Wpage.tsx
+++ wing/components/w/Wpage.tsx
@@ -1,21 +1,26 @@
 "use client"
+import dynamic from "next/dynamic";
 import Profile from "./Profile"
 import Tip from "./TipForm"
-
+import { MoveRight } from "lucide-react";
+import { useState } from "react";
+const ChartContainer = dynamic(() => import("./ChartContainer"), { ssr: false });
 function Wpage({ address }: { address: string }) {
+  const [mintAddress, setMintAddress] = useState<string>('');
   return (
     <div className="grid grid-cols-3 translate-y-15 col-span-2 min-h-[91vh] overflow-hidden">
       <div className="lg:border-r border-zinc-700 col-span-3 lg:col-span-1  grid grid-rows-5 grid-cols-1">
         <Profile address={address} />
-        <Tip address={address} mintAddress="1" />
+        <Tip address={address} mintAddress={mintAddress} />
       </div>
       <div className="col-span-3 lg:col-span-2 grid-rows-5 grid grid-cols-1">
-        <div className="px-7 py-8 border-t border-zinc-700 border-b row-span-1">
+        <div className="px-7 py-8 border-t border-zinc-700 border-b row-span-1 flex items-center gap-5 text-5xl font-bold text-zinc-200">
           <span>SOL</span>
+          <MoveRight className="text-zinc-500" />
           <span>USD</span>
         </div>
-        <div className="px-7 py-8 row-span-4 ">
-          1
+        <div className=" row-span-4 h-full ">
+          <ChartContainer />
         </div>
       </div>
     </div>
