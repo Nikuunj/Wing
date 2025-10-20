65426867a9495278a8cb0b6fb03a3aa1700c5b60
Nikunj <mnikunj262@gmail.com>
2025-10-20 17:08:33 +0530

    fix z index of popup

--- wing/components/ui/Navbar.tsx
+++ wing/components/ui/Navbar.tsx
@@ -3,11 +3,11 @@ import Image from "next/image"
 import logo from '@/public/logo.svg';
 import { Slash } from 'lucide-react';
 import { useState } from "react";
-import { BaseWalletMultiButton } from "@solana/wallet-adapter-react-ui"
-import { CONNECT_LABELS } from "@/lib/lable";
-import ConnectBtn from "./ConnectBtn";
 import { useWallet } from "@solana/wallet-adapter-react";
 import SideBar from "./SideBar";
+import dynamic from 'next/dynamic'
+
+const WalletButtonClient = dynamic(() => import('./WalletButtonClient'), { ssr: false });
 
 export const Navbar = () => {
   const [tap, setTap] = useState<boolean>(false);
@@ -26,15 +26,7 @@ export const Navbar = () => {
         </div>
         <div className={`flex  items-center relative px-12`}>
           <div className="">
-            <BaseWalletMultiButton labels={CONNECT_LABELS} style={{ outline: '0', background: 'transparent', padding: 0, height: 'fit-content' }} >
-              <ConnectBtn className="py-0.5 px-4">
-                {
-                  wallet.connected && wallet.publicKey ?
-                    wallet.publicKey.toBase58().slice(0, 3) + ".." + wallet.publicKey.toBase58().slice(-3)
-                    : 'Connect'
-                }
-              </ConnectBtn>
-            </BaseWalletMultiButton>
+            <WalletButtonClient />
           </div>
           <div className={`${tap ? '-space-y-6' : '-space-y-3'} absolute right-0 cursor-pointer transition-all duration-300 p-1 flex flex-col justify-center overl`}
             onClick={() => setTap(pre => !pre)}
@@ -43,7 +35,7 @@ export const Navbar = () => {
             <Slash className={`${tap ? 'rotate-0 h-6 w-6' : 'rotate-45 h-5 w-5'} transition-all duration-300`} />
           </div>
         </div>
-      </div>
+      </div >
       <SideBar open={tap} closeOpen={() => setTap(false)} />
     </>
   )
