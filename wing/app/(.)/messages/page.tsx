40e256b1ff615042ffd9919d51637b853f5c99bd
Nikunj <mnikunj262@gmail.com>
2025-10-27 22:34:42 +0530

    update: msg section complete

--- wing/app/(.)/messages/page.tsx
+++ wing/app/(.)/messages/page.tsx
@@ -3,6 +3,8 @@ import BorderDiv from "@/components/ui/BorderDiv";
 import { useProgram } from "@/hook/useProgram";
 import { PublicKey } from "@solana/web3.js";
 import { useQuery } from "@tanstack/react-query"
+import { Box, Clock1Icon, KeySquareIcon, MessageCircleIcon, PiggyBank, StoreIcon, User, XIcon } from "lucide-react";
+import { useState } from "react";
 
 function MessagePage() {
   const { program, publicKey, connection } = useProgram()
@@ -59,22 +61,24 @@ function MessagePage() {
 }
 
 function Message({ name, address, pubkey }: { name: string, address: string, pubkey: string }) {
+  const [open, setOpen] = useState(false);
   return (
     <BorderDiv borderSize="w-1 h-1" borderWidth={1}>
-      <div className="flex  max-w-2xl justify-between items-center w-full px-5  py-10 bg-zinc-800/60 gap-2 sm:gap-7">
-        <div>
-          <p>{name}</p>
-          <p className="text-zinc-400  break-words max-w-56 sm:max-w-full ">{address}</p>
+      <div className="flex  max-w-2xl justify-between items-center w-full px-5 sm:px-9 py-10 bg-zinc-800/60 gap-2 sm:gap-11 " >
+        <div className="space-y-2 ">
+          <p className="text-lg font-semibold">{name}</p>
+          <p className="text-zinc-400  break-words max-w-44 sm:max-w-full ">{address}</p>
         </div>
-        <p className="px-5 py-2 bg-white text-black rounded-3xl font-semibold shadow-[0px_0px_8px] shadow-amber-300">
+        <p className="px-5 py-2 bg-white text-black rounded-3xl font-semibold shadow-[0px_0px_8px] shadow-amber-300 cursor-pointer " onClick={() => setOpen(true)} >
           View
         </p>
       </div>
+      {open && <MessageOpen pubKey={pubkey} closeFn={() => { setOpen(false) }} />}
     </BorderDiv>
   )
 }
 
-function MessageOpen({ pubKey }: { pubKey: string }) {
+function MessageOpen({ pubKey, closeFn }: { pubKey: string, closeFn: () => void }) {
   const { program } = useProgram();
   async function fetchData() {
     let acc = null;
@@ -92,5 +96,39 @@ function MessageOpen({ pubKey }: { pubKey: string }) {
 
   })
 
+  return (
+    <div onClick={closeFn} className="fixed left-0 z-[100] top-0  h-full w-full flex justify-center items-center bg-zinc-900/50">
+      <div className=" max-w-2xl  w-full px-5 sm:px-9 py-10 bg-zinc-800/70 relative "
+        onClick={(e) => e.stopPropagation()}>
+        <div className="space-y-5">
+          <div className="absolute top-3.5 right-5 cursor-pointer" onClick={closeFn}><XIcon className="text-red-600" /></div>
+          <p className="flex gap-2 items-center text-lg font-semibold">
+            <User className="w-4 h-4 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" />          {data?.senderName}
+          </p>
+          <p className="flex gap-2 items-center">
+            <MessageCircleIcon className="w-4 h-4 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" />          {data?.message}
+          </p>
+          <p className="flex items-center gap-2">
+            <PiggyBank className="w-4 h-4 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" />          {data?.amount.toString()}
+          </p>
+          <p className="flex items-center gap-2">
+            <Clock1Icon className="w-4 h-4 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" /> {data?.ts.toString()}
+          </p>
+          <p className="flex gap-2 items-center break-words text-zinc-400">
+            <KeySquareIcon className="w-4 h-4 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" />{data?.senderPubkey.toString()}
+          </p>
+
+          <p className="flex items-center gap-2 text-zinc-400">
+            <Box className="w-4 h-4 drop-shadow-[0px_0px_5px] drop-shadow-amber-200" /> {pubKey}
+          </p>
+
+        </div>
+        <div className="absolute top-0 right-0 w-5 h-5 border-r border-t" />
+        <div className="absolute top-0 left-0 w-5 h-5 border-l border-t" />
+        <div className="absolute bottom-0 left-0 w-5 h-5 border-l border-b  " />
+        <div className="absolute bottom-0 right-0 w-5 h-5 border-r border-b " />
+      </div>
+    </div>
+  )
 }
 export default MessagePage
